12f9516d9861d459197f30a07442da4485c4208d
Stefano Bisio <s.b.milano@gmail.com>
2023-04-01 21:18:15 +0100

    receiving data from news API

--- src/features/search/Search.js
+++ src/features/search/Search.js
@@ -1,12 +1,48 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { fetchNews, selectSearchState } from './searchSlice';
+
 import styles from './Search.module.css';
 
 export const Search = () => {
+
+    // usestate to control the input field value  
+    const [searchValue, setSearchValue] = useState("");
+
+    const dispatch = useDispatch();
+
+    // Selector to get the API response data
+    const searchResults = useSelector(selectSearchState);
+
     return (
         <div>
-            <input type="text" placeholder="Search" />
-            <button>Search</button>
+            <input
+                value={searchValue}
+                className={styles.searchInput}
+                onChange={(e) => setSearchValue(e.target.value)}
+                placeholder='Search here' 
+            />
+            
+            {/* button that triggers the API call */}
+            <button onClick={() => dispatch(fetchNews(searchValue))}>Search</button>
+            
+            {/* Display the status and error messages */}
+            {searchResults.status === 'loading' && <p>Loading...</p>}
+            {searchResults.status === 'failed' && <p>Error: {searchResults.error}</p>}
+
+            {/* Render the raw JSON data */}
+            {searchResults.status === 'fullfilled' && (
+                <div>
+                    <p>list of articles:</p>
+                    {/* map through the articles and display some title and (foud at .results.title and .results.content) */}
+                    {searchResults.articles.results.map((article, index) => (
+                        <div key={index}>
+                            <h3>{article.title}</h3>
+                            <p>{article.content}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
---
+++ src/features/search/searchSlice.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk , createSlice } from '@reduxjs/toolkit';
+
+// Define the initial state
+const initialState = {
+    articles: [],
+    status: 'idle',
+    error: null,
+  };
+
+// Define the async thunk to fetch the news
+export const fetchNews = createAsyncThunk(
+    'search/fetchNews',
+    async (searchTerm) => {
+      const response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_1984063673ab00fc718f174a37805a897d55b&q=${searchTerm}&language=en`);
+      return await response.json();
+    }
+);
+
+// Create the slice
+const searchSlice = createSlice({
+    name: 'search',
+    initialState,
+    // Define the synchronous actions
+    reducers: {},
+    // Define the async actions
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchNews.pending, (state) => {
+          state.status = 'loading';
+        })
+        .addCase(fetchNews.fulfilled, (state, action) => {
+            state.articles = action.payload;
+            state.status = 'fullfilled';
+            state.error = null;
+        })
+        .addCase(fetchNews.rejected, (state, action) => {
+          state.status = 'failed';
+          state.error = action.payload;
+        });
+    },
+  });
+
+  // NOT needed as I don't have any sychronous actions
+// export const { updateSearch } = searchSlice.actions;
+
+// Define the selector function
+export const selectSearchState = (state) => state.search;
+
+// Export the actions and the reducer
+export default searchSlice.reducer;

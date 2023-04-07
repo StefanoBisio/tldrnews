7ddf9007e5354d0be308e9d031f94806a5c2d988
Stefano Bisio <stefano.bisio@aia.co.uk>
2023-04-07 14:08:15 +0100

    renamed Board component to Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,9 +4,9 @@ import styles from './Home.module.css';
 import { ArticlesList } from '../../features/articlesList/ArticlesList';
 import { Search } from '../../features/search/Search';
 
-export const Board = () => {
+export const Home = () => {
     return (
-        <div className={styles.board}>
+        <div className={styles.home}>
             <h1>TL;DR News</h1>
             <Search />
             <ArticlesList />
